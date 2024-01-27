085a4f366e8906b8885132cce854ef23ed87c8d6
Pun <impan_1611_2544@hotmail.com>
2024-01-27 13:12:53 +0700

    first commit

---
+++ program.py
@@ -0,0 +1,14 @@
+from tkinter import *
+
+#root window
+root=Tk()
+root.title("CheckList Application")
+root.iconbitmap("icon/check.ico")
+root.geometry("400x400+500+100")
+root.resizable(0,0)
+
+#settings
+font=("arial", 15)
+color="purple"
+root.config(bg=color)
+root,mainloop()
